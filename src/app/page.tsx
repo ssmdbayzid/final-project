b8973309ee253ce313d81c7568a695d7c890ad2c
ssmdbayzid <sharjinaakthersathi96@gmail.com>
2025-02-01 10:30:09 +0600

    add customer feedback

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,10 @@ import MostPopularBundle from "@/components/Home/MostPopulerBundle";
 import PopularCourse from "@/components/Home/PopulerCourse";
 import AllCategory from "@/components/Home/AllCategory";
 import BlogTab from "@/components/Home/BlogTab";
+import NewAddedCourse from "@/components/Home/NewAddedCourse";
+import CompletedAnHour from "@/components/Home/CompleteUnderAnHour";
+import QuizBanner from "@/components/Home/QuizBanner";
+import CustomerFeedback from "@/components/Home/CustomerFeedBack";
 
 
 export default function Home() {
@@ -13,6 +17,10 @@ export default function Home() {
         <PopularCourse />
         <AllCategory />
         <BlogTab />
+        <NewAddedCourse />
+        <CompletedAnHour />
+        <QuizBanner />
+        <CustomerFeedback />
     </div>
   );
 }
